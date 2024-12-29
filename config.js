ae313d6bc1818e6caf9cf0c3bca4c94a2fa046f8
Trevor Bollinger <tbollinger@unomaha.edu>
2024-12-29 00:54:51 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
 export const CONFIG = {
-    title: "stinktistics",
+    title: "Statistics",
 
-    hostUrl: "https://stats.stink.world",
+    hostUrl: "http://localhost:8068",
 
-    apiUrl: "https://stinkapi.boli.dev",
+    apiUrl: "http://localhost:8069",
 
-    CHANNELS_GENERAL: ["genitals", "memes", "cool-pictures", "shower-thoughts", "medical-updates", "polls", "fitness-channel", "quotes", "serious", "irl-planning", "stinkcon-iii"],
-    CHANNELS_GAMER: ["gamer-or-genitals", "giraffe-or-fuck", "tall-grass", "wizard-chat", "rimworld-stinktown", "dungeon-and-dragons"],
-    CHANNELS_MISC: ["wrapped", "nerdy-shit", "austin-and-ally", "taco-bell", "very-spooky", "r-antiwork", "fit-checks", "stink-wiki", "politichilis", "bot-shit", "dont-fucking-post-blart-here", "fantasy-football"],
-    CHANNELS_MEMEING: ["boomer", "arbies", "sherlock-fuck-yeah", "john", "copypasta", "ai-bullshit"],
-    CHANNELS_VOICE: ["voice-chat"],
-    CHANNELS_RETIRED: ["stinkcon-ii", "stinkcon-2024", "genitals-old", "general-old", "music-production-shit", "floppa-chain", "minecraft", "stinkfactory", "geralts-modification", "car-stealers", "coyboy", "holiday-alub", "quotes-madness-2021", "quotes-madness-2024", "mbwf", "sad-ass-wolves-and-edge-and-the-likes"]
-    
+    CHANNELS_GENERAL: ["general", "memes", "cool-pictures"],
+    CHANNELS_GAMER: ["gaming", "dungeon-and-dragons"]
+
 };
