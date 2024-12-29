dcec2db3b7286658959117f0bd5b35ee14b1890c
Trevor Bollinger <tbollinger@unomaha.edu>
2024-12-29 01:00:17 -0600

    config

---
+++ config.example.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+    title: "Statistics",
+
+    hostUrl: "http://localhost:8068",
+
+    apiUrl: "http://localhost:8069",
+
+    CHANNELS_GENERAL: ["general", "memes", "cool-pictures"],
+    CHANNELS_GAMER: ["gaming", "dungeon-and-dragons"]
+
+};
